617baf51673a385f1c46e94dbd2de01763f2cbe5
Deploy <deploy@deploy.com>
2017-10-05 17:33:18 -0500

    Remove reference to colors

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ export default class MinimalistPicker extends React.Component {
 
   renderOptions() {
     return this.props.options.map((item, index) => {
-      return (<Picker.Item key={index} color={Colors.black} label={item.label} value={item.value} />)
+      return (<Picker.Item key={index} color={'black'} label={item.label} value={item.value} />)
     })
   }
 
@@ -63,7 +63,7 @@ export default class MinimalistPicker extends React.Component {
   render () {
     return (
       <Animated.View style={[this.slideStyle(), { flex: 1, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }]}>
-        <TouchableOpacity onPress={() => this.closePicker()} style={{ backgroundColor: Colors.transparent, flex: 1, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, zIndex: 2 }} />
+        <TouchableOpacity onPress={() => this.closePicker()} style={{ backgroundColor: 'rgba(0, 0, 0, 0)', flex: 1, position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, zIndex: 2 }} />
         <Picker
           style={this.props.style}
           selectedValue={this.state.selectedValue}
